0e16458444c74aaf05e153190ba0fe20bd237418
Juan Douglas <juandouglas2004@gmail.com>
2021-06-15 16:33:51 -0300

    Add Api.Test

---
+++ Solution/Open.Cep.Api.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Open.Cep.Api.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
